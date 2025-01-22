ceb5cc8f63a651067f20067f2af0d689b0befd2a
exxardo <eduardoramirodematos@gmail.com>
2025-01-22 19:22:30 -0300

    feat: requisicão Get

--- src/controllers/get-users/GetUsersController.ts
+++ src/controllers/get-users/GetUsersController.ts
@@ -1,10 +1,26 @@
 import {GetUsersControllerProtocols} from "./GetUsersControllerProtocols";
 
 export class GetUsersController implements GetUsersControllerProtocols {
+
+    getUsersRepository: GetUsersRepository;
+
+    constructor(getUsersRepository: GetUsersRepository) {
+        this.getUsersRepository = getUsersRepository;
+    }
     handle() {
-        //Validar requisiçao
-        //Direcionar para o repository
-
-
+        // Validar requisiçao
+        // Direcionar para o repository
+        try: {
+            const users = await this.getUsersRepository.getUsers();
+            return {
+                statusCode: 200,
+                body: users
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: "Error"
+            }
+        }
     }
 }
